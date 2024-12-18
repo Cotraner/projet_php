18abdca2433205d0e9ca39784286c171336d8191
Clement Robin <clement.robin@isen-ouest.yncrea.fr>
2024-12-18 19:57:46 +0100

    a

--- test.php
+++ test.php
@@ -1,4 +1,16 @@
-<?php
-    echo 'oui';
-    
-?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width" />
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+    <!-- Navbar content -->
+    </nav>
+</body>
+</html>
